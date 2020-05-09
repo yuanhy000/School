ae732c3f27d3d2558bf46c93740050be22696daf
yuanhy000 <44021993+yuanhy000@users.noreply.github.com>
2020-05-09 16:40:51 +0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-	api_base_url: 'http://school.test/api/',
-	uploadImageUrl: 'https://community-storage.oss-cn-hangzhou.aliyuncs.com/',
-	AccessKeySecret: 'wiRIdhRvXwC2ZEasZQOLgM88S3X7sn',
-	OSSAccessKeyId: 'LTAIC5QjlnsVbLA4',
+	api_base_url: '',
+	uploadImageUrl: '',
+	AccessKeySecret: '',
+	OSSAccessKeyId: '',
 	timeout: 80000, //这个是上传文件时Policy的失效时间
-	mapkey: 'e6385f2595bf744cc4b92374eb502d49'
+	mapkey: ''
 }
 
 export {
